f574ea5f556fc0a2d6430247e9c783b62615edf3
jaime <jaime@vaio>
2016-10-03 12:26:08 -0500

    Menu

--- src/fp/menu/FPMenu.java
+++ src/fp/menu/FPMenu.java
@@ -15,10 +15,15 @@ public class FPMenu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("La opcion elegida es");
+        String menu = null;
+        
+       System.out.println("La opcion elegida es" + menu);
+       menu();
+       calculaAreas();
     }
     public static int menu (){
         int opcion;
+        do{
         Scanner entrada=new Scanner (System.in);
         System.out.println("*** Calculo de Areas ****");
         System.out.println("*** Mune ****");
@@ -26,7 +31,30 @@ public class FPMenu {
         System.out.println("1. Area de Cuadrados");
         System.out.println("2. Area de circulos");
         System.out.println("3. Area de Triangulos");
+        System.out.println("0. Sailir.");
         opcion=entrada.nextInt();
+        }while (opcion<=0 || opcion>=4);
         return opcion; 
     }
-}
+    public static float CalculaAreas(int op){
+        float area;
+        switch (op){
+            case 1:
+                area=areaCuadrado();
+                break;
+            case 2:
+                area=areaCirculo();
+                break;
+            case 3:
+                area=areaTriangulo();
+                break;
+            case 4:
+                area=mensajeSalida();
+                    
+                }
+        return area;
+        }
+    public static area(){
+        
+    }
+    }
