1051ceca6b0a71c204a228feaa901200aa132caa
jaime <jaime@vaio>
2016-10-03 11:55:30 -0500

    Menu

--- src/fp/menu/FPMenu.java
+++ src/fp/menu/FPMenu.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package fp.menu;
-
+import java.util.*;
 /**
  *
  * @author jaime
@@ -15,7 +15,18 @@ public class FPMenu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("La opcion elegida es");
     }
-    
+    public static int menu (){
+        int opcion;
+        Scanner entrada=new Scanner (System.in);
+        System.out.println("*** Calculo de Areas ****");
+        System.out.println("*** Mune ****");
+        System.out.println("Introduce la opcion deseada");
+        System.out.println("1. Area de Cuadrados");
+        System.out.println("2. Area de circulos");
+        System.out.println("3. Area de Triangulos");
+        opcion=entrada.nextInt();
+        return opcion; 
+    }
 }
