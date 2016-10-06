5429eb8516b229c801da850d07cb847ea12a1ac7
jaime <jaime@vaio>
2016-10-06 12:22:27 -0500

    Fp-Menu version 2

--- src/fp/menu/FPMenu.java
+++ src/fp/menu/FPMenu.java
@@ -15,12 +15,16 @@ public class FPMenu {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String menu = null;
-        
-       System.out.println("La opcion elegida es" + menu);
-       menu();
-       calculaAreas();
-       
+        //Forma 1
+      /*int m;
+      float a;
+      m=menu();
+      a=calculaAreas(m);
+      muestraResultado(a,m);*/
+      //forma
+      muestraResultado(calculaAreas(menu()));
+      
+      
        
     }
     public static int menu (){
@@ -28,7 +32,7 @@ public class FPMenu {
         do{
         Scanner entrada=new Scanner (System.in);
         System.out.println("*** Calculo de Areas ****");
-        System.out.println("*** Mune ****");
+        System.out.println("*** Menu ****");
         System.out.println("Introduce la opcion deseada");
         System.out.println("1. Area de Cuadrados");
         System.out.println("2. Area de circulos");
@@ -38,21 +42,21 @@ public class FPMenu {
         }while (opcion<=0 || opcion>=4);
         return opcion; 
     }
-    public static float CalculaAreas(int op){
+    public static float calculaAreas(int op){
         float area;
         switch (op){
             case 1:
                 area=areaCuadrado();
-               return area;
+                break;
             case 2:
                 area=areaCirculo();
                 break;
             case 3:
                 area=areaTriangulo();
                 break;
-            case 4:
-                area=mensajeSalida();
-                    
+            default:
+                mensajeSalida();
+                area=0;    
                 }
         return area;
         }
@@ -70,18 +74,43 @@ public class FPMenu {
         area = (float) Math.pow(a, 2); 
         return area;
 }
-    public static double areaTriangulo(int a, int b){
-        int areaTriangulo= (a*b)/2;
-        return areaTriangulo;
+    
+   public static float areaCirculo(){
+       float area;
+       double r;
+       final double pi;
+       pi= 3.1416;
+       r= solicitarDatos ("el radio");
+       area= (float) (pi*Math.pow(r,2));
+       return area;
+   }
+   public static float areaTriangulo(){
+        float area;
+        double a,b;   
+        a= solicitarDatos ("altura");
+        b= solicitarDatos ("base");
+        area=(float) ((a*b)/2);
+        return area;
         
     }
-   public static double areaCirculo(int r, int p){
-        double pi = 3.1416;
-       int areaCirculo= (int) (pi*Math.pow(r,2));
-       return areaCirculo;
-   }
-   public static void MensajeSalida(){
+   public static void mensajeSalida(){
        System.out.println("Error");
+       System.exit(0);
+   }
+   public static void muestraResultado (float area){
+       System.out.println("El area es: " + area);
+   }
+   public static void muestraResultado(float area, int r){
+       String figura;
+       if (r == 1) { figura= "cuadrado ";}
+       else{
+               if (r == 2) {figura= "Circulo ";
+               } else{ figura= "triangulo ";
+                 
+               }
+       }
+       System.out.println("el area del " + figura + "es igual a: " + area);
+       
    }
 
     
